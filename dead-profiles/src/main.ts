3e69a99ecb9cc9b13c7b1bf1dbe52aa731474ef4
salty-muffin <t.babel@gmx.com>
2024-09-13 12:36:16 +0200

    added randomness

--- dead-profiles/src/main.ts
+++ dead-profiles/src/main.ts
@@ -1,14 +1,42 @@
 import './style.css';
 import { paths } from './profiles.json';
 
-const SCROLLINTERVAL = 20;
-const MAXSCROLL = 500;
-const DELAY = 5000; // 5 seconds delay
+const scrollInterval = { min: 5, max: 30 };
+let currentScrollInterval = 0;
+const scrollBy = { min: -2, max: 2 };
+let currentScrollBy = 0;
+const maxScrollTime = { min: 8000, max: 20000 };
+let currentMaxScrollTime = 0;
+let currentStartTime = 0;
+const delay = { min: 10, max: 50 }; // 5 seconds delay
+
+const resetChance = 0.001;
 
 let currentIndex = 0;
 let scrollFinished = true;
 
 const frame = document.getElementById('frame') as HTMLIFrameElement | null;
+
+const getRandomInt = (attribute: { min: number; max: number }) => {
+	const min = Math.ceil(attribute.min);
+	const max = Math.floor(attribute.max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const resetValues = () => {
+	// set random values
+	currentScrollInterval = getRandomInt(scrollInterval);
+	currentScrollBy = getRandomInt(scrollBy);
+
+	console.log(
+		'currentScrollInterval',
+		currentScrollInterval,
+		'currentScrollBy',
+		currentScrollBy,
+		'currentMaxScrollTime',
+		currentMaxScrollTime
+	);
+};
 
 const loadNextFile = (files: string[]) => {
 	if (frame) {
@@ -19,7 +47,17 @@ const loadNextFile = (files: string[]) => {
 			currentIndex++;
 			if (currentIndex >= paths.length) currentIndex = 0;
 			frame.onload = () => {
-				setTimeout(scrollThrough, DELAY);
+				// start at random point in the page
+				if (frame.contentWindow)
+					frame.contentWindow.scrollBy(
+						0,
+						getRandomInt({ min: 0, max: frame.contentWindow.document.body.scrollHeight })
+					);
+
+				resetValues();
+				currentMaxScrollTime = getRandomInt(maxScrollTime);
+				currentStartTime = new Date().getTime();
+				setTimeout(scrollThrough, getRandomInt(delay));
 			};
 		}
 		setTimeout(() => loadNextFile(files), 100); // check again in 100 milliseconds, if the page has finished scrolling
@@ -28,24 +66,18 @@ const loadNextFile = (files: string[]) => {
 
 const scrollThrough = () => {
 	if (frame?.contentWindow) {
-		console.log(frame.contentWindow?.document.body.scrollHeight);
 		if (
 			frame.contentWindow!.scrollY + document.body.offsetHeight >=
 				frame.contentWindow!.document.body.scrollHeight ||
-			frame.contentWindow!.scrollY >= MAXSCROLL
+			frame.contentWindow!.scrollY <= 0 ||
+			new Date().getTime() >= currentStartTime + currentMaxScrollTime
 		) {
 			scrollFinished = true;
 		} else {
-			frame.contentWindow.scrollBy(0, 1);
-			setTimeout(scrollThrough, SCROLLINTERVAL);
+			frame.contentWindow.scrollBy(0, currentScrollBy);
+			if (Math.random() <= resetChance) resetValues();
+			setTimeout(scrollThrough, currentScrollInterval);
 		}
-
-		frame.contentWindow.document.onscroll = () => {
-			console.log(
-				frame.contentWindow!.scrollY,
-				frame.contentWindow!.scrollY + document.body.offsetHeight
-			);
-		};
 	}
 };
 
